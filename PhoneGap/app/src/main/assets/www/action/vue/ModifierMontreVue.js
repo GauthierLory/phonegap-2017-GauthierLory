8b0a66db066d1742e8a2f5de683935697e76468f
GauthierLory <gauthier.axel.lory@gmail.com>
2017-11-13 21:43:45 -0500

    modif

--- PhoneGap/app/src/main/assets/www/action/vue/ModifierMontreVue.js
+++ PhoneGap/app/src/main/assets/www/action/vue/ModifierMontreVue.js
@@ -1,45 +1,52 @@
-var ModifierMontreVue = function ()
+var ModifierMontreVue = function (montre)
 {
     instance = this;
-    this.montre = null;
+    this.montreModifie = montre;
 
     this.afficher = function ()
     {
-    $("body").html(ModifierMontreVue.pageModifierMontre);
 
-    var montreNom = '<label for="nom">Nom</label><input type="text" name="nom" id="nom" value ="'+ montre.nom +'"/>';
-    $("#nom").html(montreNom);
+        $("body").html(ModifierMontreVue.pageModifierMontre);
 
-    var montreMarque = '<label for="marque">marque</mark></label><input type="text" name="marque" id="marque" value ="'+  montre.marque +'"/>';
-    $("#marque").html(montreCouleur);
+        var montreNom = '<label for="nom">Nom</label><input type="text" name="nom" id="nom" value ="'+ montre.nom +'"/>';
+        $("#nom").html(montreNom);
 
-    var montreDescription = '<label for="description">Description</label><textarea name="description" id="description" value ="'+ montre.description +'"/>';
-    $("#description").html(montreDescription);
+        var montreMarque = '<label for="marque">marque</mark></label><input type="text" name="marque" id="marque" value ="'+  montre.marque +'"/>';
+        $("#marque").html(montreCouleur);
 
-    var montreTaille = '<label for="taille">taille</label><input type="text" name="taille" id="taille value ="'+ montre.taille +'"/>';
-    $("#taille").html(montreTaille);
+        var montreDescription = '<label for="description">Description</label><textarea name="description" id="description" value ="'+ montre.description +'"/>';
+        $("#description").html(montreDescription);
 
-    var montreReference = '<label for="reference">reference</label><input type="text" name="reference" id="reference" value ="'+ montre.reference +'"/>';
-    $("#reference").html(montreReference);
+        var montreTaille = '<label for="taille">taille</label><input type="text" name="taille" id="taille value ="'+ montre.taille +'"/>';
+        $("#taille").html(montreTaille);
 
-    var montrePrix = '<label for="prix">Prix</label><input type="text" name="prix" id="prix" value ="'+ montre.prix +'"/>';
-    $("#prix").html(montrePrix);
+        var montreReference = '<label for="reference">reference</label><input type="text" name="reference" id="reference" value ="'+ montre.reference +'"/>';
+        $("#reference").html(montreReference);
+
+        var montrePrix = '<label for="prix">Prix</label><input type="text" name="prix" id="prix" value ="'+ montre.prix +'"/>';
+        $("#prix").html(montrePrix);
 
 
 
-        $("#formulaire-modifier").on("submit", function (evenement)
-        {
-            evenement.preventDefault();
-            var nom = $("#nom").val();
-            var marque = $("#marque").val();
-            var description = $("#description").val();
-            var taille = $("#taille").val();
-            var reference = $("#reference").val();
-            var prix = $("#prix").val();
+            $("#formulaire-modifier").on("submit", function (evenement)
+            {
+                evenement.preventDefault();
+                var nom = $("#nom").val();
+                instance.montreModifier.nom = nom;
+                var marque = $("#marque").val();
+                instance.montreModifier.marque= marque;
+                var description = $("#description").val();
+                instance.montreModifier.description = description;
+                var taille = $("#taille").val();
+                instance.montreModifier.taille = taille;
+                var reference = $("#reference").val();
+                instance.montreModifier.reference = re;
+                var prix = $("#prix").val();
+                instance.montreModifier.prix = prix;
 
-              instance.montre = new Montre(nom, marque, description, taille, reference,prix)
-            window.location.hash = "#ModifierMontreVue:NouveauMontre";
-        });
+                 
+                window.location.hash = "#ModifierMontreVue:NouveauMontre";
+            });
     }
 }
 ModifierMontreVue.pageModifierMontre = $("#page-modifier-montre").html();
